e418d3098cc3653075c2a71432c5f0f764dad6ef
sadhu <1714222385@qq.com>
2022-04-28 23:17:49 +0800

    feat: 简单的状态管理器

---
+++ redux.js
@@ -0,0 +1,28 @@
+const createStore = function (initState) {
+  let state = initState;
+  let listeners = [];
+
+  /* 订阅 */
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
+  function changeState(newState) {
+    state = newState;
+    /* 通知 */
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i];
+      listener();
+    }
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    subscribe,
+    changeState,
+    getState
+  }
+}
